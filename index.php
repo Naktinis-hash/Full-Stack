dbab8d44d48ab3d8f95535cccb5ef31960844254
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2020-02-25 11:45:17 +0000

    Pirmas

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
 <?php
+$distance = rand(500, 1000);
+$consumption = 7.5;
+$price_1 = 1.3;
+$fuel_total = round($distance / $consumption, 2);
+$price_trip = round($fuel_total/$price_1, 2);
+$h1 = "Keliones skaiciuokle";
+$li_1 = "Nuvaziuota distancija $distance";
+$li_2 = "Sunaudota $fuel_total l kuro";
+$li_3 = "Kaina $price_trip pinigu";
+?>
+
+<html>
+<head>
+    <title>Trash</title>
+</head>
+<body>
+<h1><?php print $h1; ?></h1>
+<ul>
+    <li><?php print $li_1; ?></li>
+    <li><?php print $li_2; ?></li>
+    <li><?php print $li_3; ?></li>
+</ul>
+</body>
