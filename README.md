fb1bff8ad815cafd0da76eca02095d2a0d5eb08f
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2020-02-25 13:14:10 +0000

    Veikiantis page

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-Changelog (25/20/2020):
+Changelog (25/02/2020 12:01) v0.11:
 
 -Readme.md now has content (this text).
 
 -It's snowing outside.
+
+Changelog (25/02/2020 12:40) v0.12:
+
+-Dev branch added.
+
+Changelog (25/02/2020 13:00) v0.13:
+
+-Mustang added in Index.
+
+-Dacia added in Index.
--- index.php
+++ index.php
@@ -8,11 +8,36 @@ $h1 = "Keliones skaiciuokle";
 $li_1 = "Nuvaziuota distancija $distance";
 $li_2 = "Sunaudota $fuel_total l kuro";
 $li_3 = "Kaina $price_trip pinigu";
+
+$class = 'mustang';
 ?>
+
+<style>
+    div {
+        width: 200px;
+        height: 300px;
+        background-size: cover;
+    }
+    .mustang {
+        background-image: url("https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/640x400/quality/80/https://s.aolcdn.com/commerce/autodata/images/USC80FOC051A021001.jpg");
+    }
+</style>
 
 <html>
 <head>
     <title>Greitis & jega</title>
+<<<<<<< HEAD
+=======
+    <style>
+        .dacia {
+            height: 500px;
+            width: 500px;
+            background-image: url("https://www.lautra.lt/images/default/source/gallery/sandero-gall-3216.jpg");
+            background-size: cover;
+            background-position: 60%;
+        }
+    </style>
+>>>>>>> dev
 </head>
 <body>
 <h1><?php print $h1; ?></h1>
@@ -21,4 +46,6 @@ $li_3 = "Kaina $price_trip pinigu";
     <li><?php print $li_2; ?></li>
     <li><?php print $li_3; ?></li>
 </ul>
+<div class="dacia"></div>
+<div class="<?php print $class; ?>"></div>
 </body>
